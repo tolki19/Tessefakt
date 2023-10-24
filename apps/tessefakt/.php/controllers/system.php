2ee01cd6f915ae14a160720571ab5791800628ff
tolki19 <florian.kerl@gadvelop.de>
2023-10-24 13:32:53 +0200

    Prepared install class

--- apps/tessefakt/.php/controllers/system.php
+++ apps/tessefakt/.php/controllers/system.php
@@ -45,20 +45,6 @@ class system extends \tessefakt\controller{
 		$this->tessefakt->response->recommendation='login';
 	}
 	public function auth(){
-$this->dbs->current->query('insert into _users set id=default');
-$user_id=$this->dbs->current->insert();
-$this->dbs->current->query('insert into _user_emails set _user='.$user_id.',valid_from=curdate(),email="florian.kerl@gadvelop.de"');
-$user_email=$this->dbs->current->insert();
-$this->dbs->current->query('insert into _user_passwords set _user='.$user_id.',valid_from=curdate(),hash="'.$this->hash->create('Sxuyq783!').'",`date`=curdate()');
-$user_password=$this->dbs->current->insert();
-$this->dbs->current->query('insert into _user_uids set _user='.$user_id.',valid_from=curdate(),uid="Florian",date=curdate()');
-$user_uid=$this->dbs->current->insert();
-$this->dbs->current->query('insert into _user_activities set _user='.$user_id.',timestamp=now(),activity="email_create",value='.$user_email);
-$this->dbs->current->query('insert into _user_activities set _user='.$user_id.',timestamp=now(),activity="email_activate",value='.$user_email);
-$this->dbs->current->query('insert into _user_activities set _user='.$user_id.',timestamp=now(),activity="password_create",value='.$user_password);
-$this->dbs->current->query('insert into _user_activities set _user='.$user_id.',timestamp=now(),activity="password_activate",value='.$user_password);
-$this->dbs->current->query('insert into _user_activities set _user='.$user_id.',timestamp=now(),activity="uid_create",value='.$user_uid);
-$this->dbs->current->query('insert into _user_activities set _user='.$user_id.',timestamp=now(),activity="uid_activate",value='.$user_uid);
 		if(!$this->tessefakt->request->header->Authorization) return false;
 		\preg_match('#^(basic):(\S+)#is',$this->tessefakt->request->header->Authorization,$matches);
 		switch(\strtolower($matches[1])){
