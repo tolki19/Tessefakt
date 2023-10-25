d02ee5ebb8a79e52fdd590b2e747752708b49381
tolki19 <florian.kerl@gadvelop.de>
2023-10-25 20:36:21 +0200

    Improving key generator

--- .php/key.php
+++ .php/key.php
@@ -9,7 +9,8 @@ class key{
 		$this->_oApp=$app;
 		$this->__aSetup=$setup;
 	}
-	public function create(int $length):string{
-		return bin2hex(random_bytes($n));
+	public function create(int $length=16):string{
+		if($length<8||$length>256) throw new \Exception('Length out of range');
+		return bin2hex(random_bytes($length));
 	}
 }
