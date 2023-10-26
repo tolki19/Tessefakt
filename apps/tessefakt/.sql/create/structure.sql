60dfa697cbc1f7bacfd45068065bb1f66bdafbcc
tolki19 <florian.kerl@gadvelop.de>
2023-10-26 12:22:54 +0200

    Extended SQL structure

--- apps/tessefakt/.sql/create/structure.sql
+++ apps/tessefakt/.sql/create/structure.sql
@@ -331,7 +331,7 @@ DROP TABLE IF EXISTS `_user-email-state`;
 CREATE TABLE `_user-email-state` (
   `id` int(10) unsigned not null auto_increment,
   `_user-email` int(10) unsigned not null,
-  `state` enum('pending','copied','cancelled'),
+  `state` enum('waiting''pending','copied','cancelled'),
   `timestamp` datetime not null,
   `remark` text null,
   `key` varchar(64),
