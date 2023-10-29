12e78cf32b8ba708bf5159c39cf1c674ceab9366
tolki19 <florian.kerl@gadvelop.de>
2023-10-29 16:13:03 +0100

    Bug fixing

--- .php/controller_router.php
+++ .php/controller_router.php
@@ -10,7 +10,7 @@ class controller_router{
 	}
 	public function __get(string $key){
 		if(array_key_exists($key,$this->__aControllers)) return $this->__aControllers[$key];
-		include($this->__oApp->dir.'/controllers/'.$key.'.php');
+		include($this->__oApp->dir.\DIRECTORY_SEPARATOR.'controllers'.\DIRECTORY_SEPARATOR.$key.'.php');
 		$sClass='\tessefakt\apps\\'.$this->__oApp->name.'\controllers\\'.$key;
 		$this->__aControllers[$key]=new $sClass($this->__oTessefakt,$this->__oApp);
 		return $this->__aControllers[$key];
