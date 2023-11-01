b1f4a3b913f13a39e930487d89d86576606c575a
tolki19 <florian.kerl@gadvelop.de>
2023-11-01 04:31:21 +0100

    Refactored install routine

--- apps/tessefakt/.php/controllers/install.php
+++ apps/tessefakt/.php/controllers/install.php
@@ -2,21 +2,19 @@
 namespace tessefakt\apps\tessefakt\controllers;
 class install extends \tessefakt\controller{
 	public function setup(){
+		if(!key_exists('tessefakt',$this->tessefakt->setup['apps'])) throw new \Exception('App not configured');
 		$aSettings=[];
 		// foreach(whatever){
 		// 	$aSettings=$this->app->settings->create();
 		// }
-		$aApps=[];
-		foreach($this->tessefakt->setup['apps'] as $sKey=>$aApp){
-			$aApps[$sKey]=$this->app->apps->create([
-				'key'=>$sKey,
-				'name'=>$aApp['app']['name'],
-				'major'=>$aApp['version']['major'],
-				'minor'=>$aApp['version']['minor'],
-				'build'=>$aApp['version']['build'],
-				'caption'=>$aApp['version']['caption']
-			]);
-		}
+		$aApps['tessefakt']=$this->app->apps->create([
+			'key'=>'tessefakt',
+			'name'=>$this->tessefakt->setup['apps']['tessefakt']['app']['name'],
+			'major'=>$this->tessefakt->setup['apps']['tessefakt']['version']['major'],
+			'minor'=>$this->tessefakt->setup['apps']['tessefakt']['version']['minor'],
+			'build'=>$this->tessefakt->setup['apps']['tessefakt']['version']['build'],
+			'caption'=>$this->tessefakt->setup['apps']['tessefakt']['version']['caption']
+		]);
 		$aGroups['admin']=$this->app->groups->create(
 			$aApps,
 			[
