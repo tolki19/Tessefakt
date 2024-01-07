0aace479515368ae601befa216d5dea79acac148
tolki19 <florian.kerl@gadvelop.de>
2024-01-07 13:13:28 +0100

    Calling hebaz installation routine

--- .php/handlers/plain.php
+++ .php/handlers/plain.php
@@ -21,6 +21,7 @@ class plain extends \tessefakt\handler{
 		$this->response->op['urls']['folder']=compileurl($this->tessefakt->setup['urls']['folder']);
 		$this->response->op['urls']['target']=compileurl($this->tessefakt->setup['urls']['target']);
 $this->apps->tessefakt->libraries->install->create_structure();
+$this->apps->hebaz->libraries->install->create_structure();
 		$sApp=$this->setup['defaults']['app'];
 		$sEntrance='plain';
 		$sController=$this->setup['apps'][$sApp]['defaults']['entrances'][$sEntrance]['controller'];
