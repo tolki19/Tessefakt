200bb73ccbc980e1ec784449fea2949c66365669
tolki19 <florian.kerl@gadvelop.de>
2024-01-07 13:50:07 +0100

    Removed overhead tessefakt libraries

--- apps/tessefakt/.php/libraries/apps.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace tessefakt\apps\tessefakt\controllers;
-class apps extends \tessefakt\library{
-	public function create(array $data):array{
-		$aReturn=[];
-		foreach($data as $aApp) $aReturn[]=$this->app->apps->create($aApp);
-		return $aReturn;
-	}
-}
--- apps/tessefakt/.php/libraries/users.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace tessefakt\apps\tessefakt\controllers;
-class users extends \tessefakt\library{
-	public function create(array $data):array{
-		$aReturn=[];
-		foreach($data as $aUser) $aReturn[]=$this->user->create($aUser);
-		return $aReturn;
-	}
-}
