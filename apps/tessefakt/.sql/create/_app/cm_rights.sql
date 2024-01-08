37e0c80f28974586192a24b900f22d03ed6f4cd4
tolki19 <florian.kerl@gadvelop.de>
2024-01-08 06:17:56 +0100

    Reengineered tessefakt rights dumps

--- apps/tessefakt/.sql/create/_app/cm_rights.sql
+++ apps/tessefakt/.sql/create/_app/cm_rights.sql
@@ -2,10 +2,15 @@ DROP TABLE IF EXISTS `_app-cm_rights`;
 CREATE TABLE `_app-cm_rights` (
 	`id` int(10) UNSIGNED NOT NULL auto_increment,
 	`_app` int(10) unsigned NOT NULL,
+	`_group` int(10) UNSIGNED NULL,
+	`_user` int(10) UNSIGNED NULL,
 	`controller` varchar(64) not null,
 	`method` varchar(64) null,
+	`right_execute` tinyint(1) NULL,
 	primary key(`id`),
 	key(`_app`),
+	key(`_group`),
+	key(`_user`),
 	key(`controller`),
 	key(`method`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci comment="tessefakt_13.0-tessefakt_13.0";
--- apps/tessefakt/.sql/create/_app/tpl_rights.sql
+++ apps/tessefakt/.sql/create/_app/tpl_rights.sql
@@ -2,9 +2,15 @@ DROP TABLE IF EXISTS `_app-tpl_rights`;
 CREATE TABLE `_app-tpl_rights` (
 	`id` int(10) UNSIGNED NOT NULL auto_increment,
 	`_app` int(10) unsigned NOT NULL,
+	`_group` int(10) UNSIGNED NULL,
+	`_user` int(10) UNSIGNED NULL,
 	`tpl` varchar(255) NOT NULL,
 	`div` varchar(255) NULL,
+	`right_display` tinyint(1) NULL,
+	`right_iput` tinyint(1) NULL,
 	primary key(`id`),
 	key(`_app`),
+	key(`_group`),
+	key(`_user`),
 	key(`tpl`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci comment="tessefakt_13.0-tessefakt_13.0";
