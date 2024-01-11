45bac2b86a3943e33ce9bfe7ef0fce10d4552d02
tolki19 <florian.kerl@gadvelop.de>
2024-01-11 12:05:35 +0100

    Improved tessefakt sl dumps and libraries

--- apps/tessefakt/.php/libraries/settings.php
+++ apps/tessefakt/.php/libraries/settings.php
@@ -3,18 +3,24 @@ namespace tessefakt\apps\tessefakt\libraries;
 class settings extends \tessefakt\library{
 	public function create(array $data):int{
 		$iSetting=$this->_create(
+			$data['_app']??null,
+			$data['_group']??null,
+			$data['_user']??null,
 			$data['key'],
 			$data['caption'],
 			$data['keywords'],
 			$data['value'],
-			$data['remark']
+			$data['remark']??null
 		);
 		return $iSetting;
 	}
-	protected function _create(string $key,string $caption,string $keywords,string $value,?string $remark):int{
+	protected function _create(int|null $app,int|null $group,int|null $user,string $key,string $caption,string $keywords,string $value,string|null $remark):int{
 		$this->connectors->db->query('
 			insert into `_settings`
 			set 
+				`_app`='.($app??'null').',
+				`_group`='.($group??'null').',
+				`_user`='.($user??'null').',
 				`key`="'.$this->connectors->db->escape($key).'",
 				`caption`="'.$this->connectors->db->escape($caption).'",
 				`keywords`="'.$this->connectors->db->escape($keywords).'",
--- apps/tessefakt/.sql/create/_settings.sql
+++ apps/tessefakt/.sql/create/_settings.sql
@@ -2,9 +2,12 @@ DROP TABLE IF EXISTS `_settings`;
 CREATE TABLE `_settings` (
 	`id` int(10) UNSIGNED NOT NULL auto_increment,
 	`_app` int(10) unsigned NULL,
+	`_group` int(10) UNSIGNED NULL,
+	`_user` int(10) UNSIGNED NULL,
 	`key` varchar(64) NOT NULL,
 	`caption` varchar(64) NOT NULL,
 	`keywords` text NULL,
+	`value` varchar(16) NOT NULL,
 	`remark` text NULL,
 	primary key(`id`),
 	key(`_app`),
